18085cd96b5e60767cd9c2f6d223ab1ef7b0dba4
Ryan <ryan-walker@live.com>
2021-02-28 22:06:04 -0500

    Buidling todo list in React - add todo component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+   function handleAddTodo (e){
+
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <TodoList todos = {todos}/>
+      <input type = 'text'/>
+      <button onClick = {handleAddTodo}> Add to do </button>
+      <button> Clear completed</button>
+      <div> 0 left to do</div>
+    </div>      
+
+  )
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Todo({ todo }) {
+    return (
+        <div>
+            <label>
+                <input type = "checkbox" checked = {todo.complete}></input>
+                 {todo.name}  
+            
+            </label>
+                      
+        </div>
+    )
+}
